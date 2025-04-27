9b33dab81c8f2c7f4444d751d98072b9b0803301
kalyankashaboina <kalyankashaboina07@gmail.com>
2025-04-27 11:58:47 +0530

    updated 01

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
-const {handleSocketConnection}=require("./utils/socket")
+const { handleSocketConnection } = require("./utils/socket")
 const messageRoutes = require('./routes/messageRoutes');
 const connectDB = require('./lib/mongoDb');
 
@@ -15,13 +15,11 @@ const app = express();
 // Allowed front-end origins
 const allowedOrigins = [
   //'http://localhost:5173',
-  //'https://chatty-jqz124liq-kalyankashaboinas-projects.vercel.app', 
-  //'https://chatty-bay-eight.vercel.app',
- 'https://chatty-navy.vercel.app/'
+  'https://chatty-navy.vercel.app'
 ];
 
 
-app.use(cookieParser()); 
+app.use(cookieParser());
 // CORS configuration
 app.use(cors({
   origin: (origin, callback) => {
@@ -41,7 +39,7 @@ app.use(express.json());
 
 
 app.use('/api', authRoutes);
-app.use("/chat",messageRoutes)
+app.use("/chat", messageRoutes)
 
 app.get('/welcome', (req, res) => {
   res.status(200).json({ message: 'Welcome to the Chat API!' });
